a3d390bd99a91613df7e0c23c717285aba118780
Britt Selvitelle <yap@bri.tt>
2015-08-10 00:26:49 -0700

    more setup

--- setup.sh
+++ setup.sh
@@ -6,11 +6,14 @@ ln -sf ~/dotfiles/oh-my-zsh/themes ~/.oh-my-zsh/custom/themes
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 
 # vim
+
 ln -sf ~/dotfiles/vim ~/.vim
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # git
+git config --global user.name "Britt Selvitelle"
+git config --global user.email yap@bri.tt
 git config --global core.excludesfile ~/dotfiles/gitignore
 
 echo "start zsh"
